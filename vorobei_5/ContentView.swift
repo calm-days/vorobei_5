cd4838a62fef6c1a009de7c6c44e2abec121dad8
Roman Liukevich <Raman.Liukevich@ringieraxelspringer.pl>
2024-12-13 16:52:27 +0100

    initial commit

--- vorobei_5/ContentView.swift
+++ vorobei_5/ContentView.swift
@@ -8,14 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var location: CGPoint = CGPoint(x: 50, y: 50)
+
+    @GestureState private var fingerLocation: CGPoint? = nil
+    @GestureState private var startLocation: CGPoint? = nil
+
+    var simpleDrag: some Gesture {
+        DragGesture()
+            .onChanged { value in
+                var newLocation = startLocation ?? location
+                newLocation.x += value.translation.width
+                newLocation.y += value.translation.height
+                self.location = newLocation
+            }.updating($startLocation) { (value, startLocation, transaction) in
+                startLocation = startLocation ?? location
+            }
+    }
+
+    var fingerDrag: some Gesture {
+        DragGesture()
+            .updating($fingerLocation) { (value, fingerLocation, transaction) in
+                fingerLocation = value.location
+            }
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+        ZStack {
+
+            VStack(spacing: 0) {
+                Color.black
+                Color.white
+                Color.black
+                Color.white
+            }
+
+            VStack(spacing: 0) {
+                Color.white
+                Color.pink
+                Color.yellow
+                Color.black
+            }
+            .overlay {
+                RoundedRectangle(cornerRadius: 15)
+                    .blendMode(.destinationOut)
+                    .frame(width: 100, height: 100)
+                    .position(location)
+                    .gesture(simpleDrag.simultaneously(with: fingerDrag))
+            }
+            .compositingGroup()
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
 
