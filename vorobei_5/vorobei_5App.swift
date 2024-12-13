51a029eef62a864cad5ad1cda4af2a2dc3d5502b
Roman Liukevich <Raman.Liukevich@ringieraxelspringer.pl>
2024-12-13 15:03:50 +0100

    Initial Commit

---
+++ vorobei_5/vorobei_5App.swift
@@ -0,0 +1,17 @@
+//
+//  vorobei_5App.swift
+//  vorobei_5
+//
+//  Created by Raman Liukevich on 13/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct vorobei_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
